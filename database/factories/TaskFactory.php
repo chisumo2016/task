e837dc43669d15be324374be89e3f09179d0bd5e
Bernard Chisumo <bchisumo74@gmail.com>
2025-12-04 16:45:01 +0000

    feat :Add Task model and migration

---
+++ database/factories/TaskFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Task>
+ */
+class TaskFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => $this->faker->name(),
+            'user_id' => rand(1, 5), // 1-5
+            'status' => rand(1, 10),
+        ];
+    }
+}
